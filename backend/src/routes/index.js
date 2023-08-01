ebb689c56785ee9590c0886a6f6fd0e226dd68ec
Tomas <69489194+NitsugaSamot@users.noreply.github.com>
2023-08-01 12:30:28 -0300

    autenticacion en proceso

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,11 +2,15 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
+const userRoutes = require('./userRoutes')
+
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+router.use('/users', userRoutes)
 
 
 module.exports = router;
