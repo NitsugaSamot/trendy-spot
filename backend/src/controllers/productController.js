d7f0a86be51a505c2449ff04865f3dc53959c0d6
Tomas <69489194+NitsugaSamot@users.noreply.github.com>
2023-08-01 23:17:15 -0300

    get-post-products

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -1,5 +1,21 @@
 const{ Product } = require('../db')
 
+// http://localhost:3004/products
+
+const getAllProducts = async(req, res) => {
+    try {
+        const products = await Product.findAll()
+
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+        console.log(error);
+    }
+}
+
+
+// http://localhost:3004/products/create
+
 const postProduct = async (req, res) => {
         try {
                 const {name, size, price, image, description, stock} = req.body
@@ -20,4 +36,4 @@ const postProduct = async (req, res) => {
         }
 }
 
-module.exports = {postProduct}
+module.exports = {postProduct, getAllProducts}
