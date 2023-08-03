cc2cadbad62db3a0978aa59f8c0d1e0c28b611b8
LucasGonzalezRighi <lucasgonzalezrighi@gmail.com>
2023-08-03 19:13:41 -0300

    serchbar

--- frontend/src/redux/action-types.js
+++ frontend/src/redux/action-types.js
@@ -1,8 +1,4 @@
 export const GET_ALL = 'GET_ALL';
 export const ORDER_PRICE = 'ORDER_PRICE';
-<<<<<<< HEAD
-export const SEARCH_NAME = 'SEARCH_NAME';
-=======
 export const SEARCH_NAME = 'SEARCH_NAME';
 export const POST_PRODUCT = "POST_PRODUCT";
->>>>>>> dev
