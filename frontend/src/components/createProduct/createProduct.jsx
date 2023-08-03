8322eb553430788fdfb35983ba8c3f45d17d7fca
tomascallenius <tomascallenius96@gmail.com>
2023-08-03 00:42:19 -0300

    merge resuelto

--- frontend/src/components/createProduct/createProduct.jsx
+++ frontend/src/components/createProduct/createProduct.jsx
@@ -1,12 +1,7 @@
 const CreateProduct = () => {
   return (
     <div>
-<<<<<<< HEAD
-              <form action=""></form>
-              
-=======
       <h1>este es el form</h1>
->>>>>>> dev
     </div>
   );
 };
