ef754c82db1d100724ac6bab557914cc48501b71
Lokerrr <juan53@live.com.ar>
2023-08-04 01:24:08 -0300

    models and routes validates

--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-  
+
     sequelize.define('product', {
     id: {
       type: DataTypes.INTEGER,
@@ -11,30 +11,68 @@ module.exports = (sequelize) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        len: [3, 50], // solo permite entre 3 y 50 caracteres.
+        notNull: {
+          msg: 'Product name is required'
+        }
+      }
     },
     size: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate:{
+        len: [1, 5], // solo permite un talle entre 0 y 5 caracteres.
+        notNull: {
+          msg: 'Product size is required'
+        }
+      }
     },
     price: {
       type: DataTypes.INTEGER,
-      allowNull: false
-    }, 
+      allowNull: false,
+      validate:{
+        // len: [100, 100.000], // solo va permitir un precio entre 100 y 100.000.
+        min: 100, // Precio mínimo
+        max: 100000, // Precio máximo
+        isNumeric: true, // solamente tiene que ser de tipo numerico.
+        notNull: {
+          msg: 'Product price is required'
+        }
+      }
+    },
     image: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Product image is required'
+        },
+      }
     },
     description: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Product description is required'
+        },
+        len: [5, 500] // La cantidad de caracteres solo sera entre 5 y 500 caracteres
+      }
     },
     stock: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        min: 0 // Asegurar que el stock no sea negativo
+      }
     },
     color: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate:{
+        isAlpha: true, // Solo permite letras.
+      }
     },
     productbrand: {
       type: DataTypes.STRING,
@@ -44,4 +82,3 @@ module.exports = (sequelize) => {
   { timestamps: false }
   );
 };
-
