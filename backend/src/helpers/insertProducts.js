d1a49d4807f7af66794b8530bd106176ae5e2384
Lokerrr <juan53@live.com.ar>
2023-08-07 00:49:25 -0300

    commit con async

--- backend/src/helpers/insertProducts.js
+++ backend/src/helpers/insertProducts.js
@@ -1,8 +1,8 @@
 const createProduct = require('../controllers/productsControllers/createProductController');
 
 const insertProduct = async (products) => {
-    const productsList = await products.map(({name, size, price, image, description, stock, color, brand}) => {
-        createProduct(name, size, price, image, description, stock, color, brand)
+    const productsList = await products.map(async({name, size, price, image, description, stock, color, brand}) => {
+        await createProduct(name, size, price, image, description, stock, color, brand)
         return {
             name,
             size,
